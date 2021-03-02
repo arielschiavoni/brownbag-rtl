3cf15e81273b80cb375c12f913147a16a19ea5b3
Ariel Schiavoni <arielschiavoni@gmail.com>
2021-03-02 11:43:36 +0100

    add alternative implementation to fix tests for AccordionMulti

--- src/Accordion-enzyme.test.tsx
+++ src/Accordion-enzyme.test.tsx
@@ -3,9 +3,9 @@ import React from "react";
 // then please read blog.kentcdodds.com/c08851a68bb7
 import Enzyme, { mount } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import { Accordion } from "./Accordion";
+// import { Accordion } from "./Accordion";
 // import { Accordion } from "./Accordion-hooks";
-// import { Accordion } from "./AccordionMulti";
+import { Accordion } from "./AccordionMulti";
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -13,8 +13,10 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 test("setOpenIndex sets the open index state properly", () => {
   const wrapper = mount<Accordion>(<Accordion items={[]} />);
   expect(wrapper.state("openIndex")).toBe(0);
+  // expect(wrapper.state("openIndexes")).toEqual([0]);
   wrapper.instance().setOpenIndex(1);
   expect(wrapper.state("openIndex")).toBe(1);
+  // expect(wrapper.state("openIndexes")).toEqual([1]);
 });
 
 test("Accordion renders AccordionContents with the item contents", () => {
